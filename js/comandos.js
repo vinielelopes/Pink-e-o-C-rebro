6fb3a483a9e6a5e58b906eb353065e9181478150
vinielelopes <vinielelopes@outlook.com>
2022-04-29 14:02:24 -0300

    adicionando alert com nome do usuario

--- js/comandos.js
+++ js/comandos.js
@@ -2,6 +2,8 @@ const botao = document.getElementById("bot");
 
 botao.addEventListener("click", function inicio(){
     var nome = prompt("Para começar, diga seu nome: ");
+    // window.location.pathname = "/html/pag2.html";
     window.location = "file:///Users/vinielelopes/Downloads/projeto1/html/pag2.html";
+    alert("Olá, " + nome +  "! Vamos começar o jogo?")
 })
 
