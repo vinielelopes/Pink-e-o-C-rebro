e1cf1282263cc70c8aff3bc738c69f3a9a0a7954
vinielelopes <vinielelopes@outlook.com>
2022-04-29 00:41:03 -0300

    inicio

---
+++ js/comandos.js
@@ -0,0 +1,7 @@
+const botao = document.querySelector("button");
+
+botao.addEventListener("click", function inicio(){
+    var nome = prompt("Para começar, diga seu nome: ");
+    window.location = "file:///Users/vinielelopes/Downloads/projeto%201%20-%20resilia/html/pag2.html";
+})
+
