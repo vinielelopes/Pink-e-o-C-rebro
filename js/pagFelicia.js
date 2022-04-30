e76252d8ffa457cdab79edebb70bd6202424b607
vinielelopes <vinielelopes@outlook.com>
2022-04-29 22:26:06 -0300

    jogo do pink e felicia, alteracao da fonte

--- js/pagFelicia.js
+++ js/pagFelicia.js
@@ -9,4 +9,42 @@ botao.addEventListener("click", function jogoFelicia(){
         alert("Você errou! Comece de novo.");
         window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
     }
+    if (pergunta2 === "pink"){
+        alert("Você acertou!");
+        var pergunta3 = prompt("3- Onde o Cérebro trabalha?");
+    } else {
+        alert("Você errou! Comece de novo.");
+        window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
+    }
+    if (pergunta3 === "laboratorio" || pergunta3 === "laboratório"){
+        alert("Você acertou!");
+        var pergunta4 = prompt("4- Qual turno o Cérebro trabalha?");
+    } else {
+        alert("Você errou! Comece de novo.");
+        window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
+    }
+    if (pergunta4 === "noite"){
+        alert("Você acertou!");
+        var pergunta5 = prompt("5-Felicia ama ap _ _ _ _r seus ratos. ");
+    } else {
+        alert("Você errou! Comece de novo.");
+        window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
+    }
+    if (pergunta5 === "apertar"){
+        alert("Você acertou!");
+        var pergunta6 = prompt("6- Você está quase lá!! Qual desenho de animação está no rebot com Pink e o Cérebro? ")
+    } else {
+        alert("Você errou! Comece de novo.");
+        window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
+
+    }
+    if (pergunta6 === "animaniacs"){
+        alert("Você venceu!!");
+        window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/venceu.html")
+    } else {
+        alert("Você errou! Comece de novo.");
+        window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
+
+    }
 });
+
--- js/pagPink.js
+++ js/pagPink.js
@@ -9,4 +9,26 @@ botao.addEventListener("click", function jogoPink(){
         alert("Você errou! Comece de novo.");
         window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
     }
+    if (pergunta2 === "Felícia" || pergunta2 === "felicia"){
+        alert("Você acertou!");
+        var pergunta3 = prompt("3- Onde Pink e Cérebro moram?");
+    } else {
+        alert("Você errou! Comece de novo.");
+        window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
+    }
+    if (pergunta3 === "laboratorio" || pergunta3 === "laboratório"){
+        alert("Você acertou!");
+        var pergunta4 = prompt("4- Você está quase lá! O que Cérebro fará hoje a noite?");
+    } else {
+        alert("Você errou! Comece de novo.");
+        window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
+    }
+    if (pergunta4 === "tentar conquistar o mundo"){
+        alert("Você venceu!!!");
+        window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/venceu.html")
+    } else {
+        alert("Você errou! Comece de novo.");
+        window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
+    }
+
 });
