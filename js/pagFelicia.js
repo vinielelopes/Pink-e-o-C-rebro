4376815bac21986da6e8d735d9495081d5c58585
vinielelopes <vinielelopes@outlook.com>
2022-04-29 23:35:40 -0300

    alteracao jogo pink e felicia mudando para escolhas

--- js/pagFelicia.js
+++ js/pagFelicia.js
@@ -1,44 +1,44 @@
 const botao = document.getElementById("bot");
 
 botao.addEventListener("click", function jogoFelicia(){
-    var pergunta1 = prompt("1- Qual o sonho do Cérebro?");
-    if (pergunta1 === "conquistar o mundo"){
+    var pergunta1 = prompt("1- Qual o sonho do Cérebro?\n 1- ficar rico\n 2-conquistar o mundo\n 3- ser pai");
+    if (pergunta1 === "2"){
         alert("Você acertou!");
-        var pergunta2 = prompt("2- Qual o melhor amigo do Cérebro?");
+        var pergunta2 = prompt("2- Qual o melhor amigo do Cérebro?\n 1- ninguém\n 2- pink\n 3-gato");
     } else {
         alert("Você errou! Comece de novo.");
         window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
     }
-    if (pergunta2 === "pink"){
+    if (pergunta2 === "2"){
         alert("Você acertou!");
-        var pergunta3 = prompt("3- Onde o Cérebro trabalha?");
+        var pergunta3 = prompt("3- Onde o Cérebro trabalha?\n 1- escritório\n 2- laboratório \n 3- hospital");
     } else {
         alert("Você errou! Comece de novo.");
         window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
     }
-    if (pergunta3 === "laboratorio" || pergunta3 === "laboratório"){
+    if (pergunta3 === "2"){
         alert("Você acertou!");
-        var pergunta4 = prompt("4- Qual turno o Cérebro trabalha?");
+        var pergunta4 = prompt("4- Qual turno o Cérebro trabalha?\n 1- manhã\n 2- tarde\n 3- noite");
     } else {
         alert("Você errou! Comece de novo.");
         window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
     }
-    if (pergunta4 === "noite"){
+    if (pergunta4 === "3"){
         alert("Você acertou!");
-        var pergunta5 = prompt("5-Felicia ama ap _ _ _ _r seus ratos. ");
+        var pergunta5 = prompt("5-O que Felicia ama fazer com seus ratos. \n 1-apertar\n 2-brincar\n 3- dormir");
     } else {
         alert("Você errou! Comece de novo.");
         window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
     }
-    if (pergunta5 === "apertar"){
+    if (pergunta5 === "1"){
         alert("Você acertou!");
-        var pergunta6 = prompt("6- Você está quase lá!! Qual desenho de animação está no reboot com Pink e o Cérebro? ")
+        var pergunta6 = prompt("6- Você está quase lá!! Qual desenho de animação está no reboot com Pink e o Cérebro?\n 1- simpsons\n 2- animaniacs\n 3- looney tunes ")
     } else {
         alert("Você errou! Comece de novo.");
         window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
 
     }
-    if (pergunta6 === "animaniacs"){
+    if (pergunta6 === "2"){
         alert("Você venceu!!");
         window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/venceu.html")
     } else {
--- js/pagPink.js
+++ js/pagPink.js
@@ -1,29 +1,29 @@
 const botao = document.getElementById("bot");
 
 botao.addEventListener("click", function jogoPink(){
-    var pergunta1 = prompt("1- Que animal o Cérebro é?");
-    if (pergunta1 === "rato"){
+    var pergunta1 = prompt("1- Que animal o Cérebro é?\n 1- gato\n 2- rato \n 3- macaco");
+    if (pergunta1 === "2"){
         alert("Você acertou!");
-        var pergunta2 = prompt("2- Qual o nome da dona do Cérebro?");
+        var pergunta2 = prompt("2- Qual o nome da dona do Cérebro?\n 1- marisa\n 2- isa\n 3- felicia");
     } else {
         alert("Você errou! Comece de novo.");
         window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
     }
-    if (pergunta2 === "Felícia" || pergunta2 === "felicia"){
+    if (pergunta2 === "3"){
         alert("Você acertou!");
-        var pergunta3 = prompt("3- Onde Pink e Cérebro moram?");
+        var pergunta3 = prompt("3- Onde Pink e Cérebro moram?\n 1- casa\n 2- árvore \n 3- laboratório ");
     } else {
         alert("Você errou! Comece de novo.");
         window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
     }
-    if (pergunta3 === "laboratorio" || pergunta3 === "laboratório"){
+    if (pergunta3 === "3"){
         alert("Você acertou!");
-        var pergunta4 = prompt("4- Você está quase lá! O que Cérebro fará hoje a noite?");
+        var pergunta4 = prompt("4- Você está quase lá! O que Cérebro fará hoje a noite?\n 1- conquistar o mundo\n 2- dormir\n 3- estudar");
     } else {
         alert("Você errou! Comece de novo.");
         window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/index.html");
     }
-    if (pergunta4 === "tentar conquistar o mundo"){
+    if (pergunta4 === "1"){
         alert("Você venceu!!!");
         window.location = ("file:///Users/vinielelopes/Downloads/projeto1/html/venceu.html")
     } else {
