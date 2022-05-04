1ba009b0df54db512b920803585c3c601c47c9f4
vinielelopes <vinielelopes@outlook.com>
2022-05-04 19:14:09 +0400

    tentando fazer funcionar pagina2 no pages

--- js/comandos.js
+++ js/comandos.js
@@ -2,7 +2,7 @@ const botao = document.getElementById("bot");
 
 botao.addEventListener("click", function inicio(){
     var nome = prompt("Para começar, diga seu nome: ");
-    window.location.pathname = ("/html/pag2.html");
+    window.location.pathname = ("/Pink-e-o-Cerebro/html/pag2.html");
     alert("Olá, " + nome +  "! Vamos começar o jogo?")
 })
 
